81f7aa74fe0cf335fdaf1e9d4f134f07ce9b1196
kurra <sarsukurra11@gmail.com>
2020-05-05 19:55:37 -0400

    missing<

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -3,7 +3,7 @@
     <!-- HERO SECTION -->
     <section class="hero is-bold has-text-centered">
       <div class="hero-body">
-        div class="container">
+        <div class="container">
           <h1 class="title is-1 is-uppercase"> ABOUT US </h1>
         </div>
       </div>
